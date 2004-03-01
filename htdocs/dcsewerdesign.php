29b74996cddf6ccc780f51cd84ced1220575819c
sdteffen <sdteffen@97a9c476-a710-0410-a1e8-994cfe14d75d>
2004-03-01 10:28:38 +0000

    back-link, PHP fixes.
    git-svn-id: https://dcwaterdesign.svn.sourceforge.net/svnroot/dcwaterdesign/trunk@148 97a9c476-a710-0410-a1e8-994cfe14d75d

--- htdocs/dcsewerdesign.php
+++ htdocs/dcsewerdesign.php
@@ -1,36 +1,24 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
   <head>
-    <title>DC Water Design Extension</title>
+    <title>DC Sewer Design Extension</title>
     <link rel="stylesheet" href="dcwaterdesign.css" type="text/css"/>
     <link rel="shortcut icon" href="images/dcmms.ico" />
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
   </head>
   <body>
     <div class="dcmms">
-    <h1>DC Sewer Design Extension</h1>
-    <p>The DC Sewer Design Extension is currently available from the CVS.</p>
-    <p>The DC Sewer Design Extension is released under the the terms of the <a 
-      href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser General 
-      Public License</a> (LGPL).</p>
-      
-    <h2>File Releases</h2>
-    <!-- require_once 'rss_fetch.inc';
+      <h1>DC Sewer Design Extension</h1>
+      <p>The DC Sewer Design Extension is currently available from the CVS.</p>
+      <p>The DC Sewer Design Extension is released under the the terms of the <a 
+	href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser General 
+	Public License</a> (LGPL).</p>
 
-    $url = "http://sourceforge.net/export/rss2_projfiles.php?group_id=76364";
-    @$rss = fetch_rss($url);
-    if($rss) {
-      foreach ($rss->items as $item ) { 
-	if(!(strpos($item[title], "PostArabic") === false)) { ?>
-	  <h3><?= $item[title]; ?></h3>
-	  <p><?= $item["description"]; ?></p><?php
-	}
-      }
-    } else { ?>
-      <p class="important">RSS feed currently unavailable. Please check again
-        later.</p> <?php
-    } --></div>
+      <h2>File Releases</h2>
+      <p>An installer for the extension will be released soon.</p>
+      <p>Back to the <a href="index.php">DC Water Design Extension</a> page.</p>
+    </div>
   <p class="footer">$Date$ <a href=
    "mailto:Steffen_Macke@dorsch.com.jo">Steffen Macke 
    &lt;Steffen_Macke@dorsch.com.jo&gt;</a></p>
